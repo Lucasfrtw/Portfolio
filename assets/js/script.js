228e807e03b3734c7283883beda8a8879ccffe5a
Lucasfrtw <115669144+Lucasfrtw@users.noreply.github.com>
2023-03-02 17:31:06 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
 
-navLinks.addEventListener('click', () =>{
-  navLinks.classList.toggle('nav-active')
-})
-
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('nav-active');
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("nav-active");
 });
 
+const navLink = document.querySelectorAll(".nav-links li a");
 
-
+navLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    const navMenu = document.querySelector(".nav-links");
+    navMenu.classList.remove("nav-active");
+    hamburger.classList.remove("toggle");
+  });
+});
